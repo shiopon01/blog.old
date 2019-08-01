b5ae9210791881928daa9b785e12a9273453b210
shiopon01 <shio.0323@gmail.com>
2019-08-01 18:27:55 +0900

    add configs

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/detail": { page: "/detail" },
+      "/initial-props": { page: "/initial-props" }
+    };
+  }
+};
