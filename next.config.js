1cc97d496a5452da0d6a76dd571f7a2741541fd7
shiopon01 <shio.0323@gmail.com>
2019-08-18 01:35:23 +0900

    modified: next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ module.exports = {
     return {
       "/": { page: "/" },
       "/about": { page: "/about" },
-      "/page": { page: "/page" },
-      "/entry": { page: "/entry" },
-      "/initial-props": { page: "/initial-props" }
+      "/initial-props": { page: "/initial-props" },
+
+      "/page/1": { page: "/page/[pid]" },
+      "/entry/2019-01-01-test": { page: "/entry/[entry]" }
     };
   }
 };
