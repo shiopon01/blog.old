c8cb27e8f82a7f2c985c2c70d59abdd26037db18
shiopon01 <shio.0323@gmail.com>
2019-09-04 00:28:32 +0900

    index編集

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,96 +1,37 @@
 import React from "react";
-// import Link from "next/link";
+import Link from "next/link";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
+import { makeStyles } from "@material-ui/core/styles";
+
+import { Link as MLink } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
-  toolbar: {
-    borderBottom: `1px solid ${theme.palette.divider}`,
-  },
-  toolbarTitle: {
-    flex: 1,
-  },
   toolbarSecondary: {
-    justifyContent: 'space-between',
-    overflowX: 'auto',
+    justifyContent: "space-between",
+    overflowX: "auto"
   },
   toolbarLink: {
     padding: theme.spacing(1),
-    flexShrink: 0,
-  },
-  mainFeaturedPost: {
-    position: 'relative',
-    backgroundColor: theme.palette.grey[800],
-    color: theme.palette.common.white,
-    marginBottom: theme.spacing(4),
-    backgroundImage: 'url(https://source.unsplash.com/user/erondu)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-  },
-  overlay: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    backgroundColor: 'rgba(0,0,0,.3)',
-  },
-  mainFeaturedPostContent: {
-    position: 'relative',
-    padding: theme.spacing(3),
-    [theme.breakpoints.up('md')]: {
-      padding: theme.spacing(6),
-      paddingRight: 0,
-    },
-  },
-  mainGrid: {
-    marginTop: theme.spacing(3),
-  },
-  card: {
-    display: 'flex',
-  },
-  cardDetails: {
-    flex: 1,
-  },
-  cardMedia: {
-    width: 160,
-  },
-  markdown: {
-    ...theme.typography.body2,
-    padding: theme.spacing(3, 0),
-  },
-  sidebarAboutBox: {
-    padding: theme.spacing(2),
-    backgroundColor: theme.palette.grey[200],
-  },
-  sidebarSection: {
-    marginTop: theme.spacing(3),
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    marginTop: theme.spacing(8),
-    padding: theme.spacing(6, 0),
-  },
+    flexShrink: 0
+  }
 }));
 
 export default function MyAppBar() {
   const classes = useStyles();
 
   const sections: string[] = [
-    'Technology',
-    'Design',
-    'Culture',
-    'Business',
-    'Politics',
-    'Opinion',
-    'Science',
-    'Health',
-    'Style',
-    'Travel',
+    "about",
+    "design",
+    "culture",
+    "business",
+    "politics",
+    "opinion",
+    "science",
+    "health",
+    "style",
+    "travel"
   ];
 
   return (
@@ -103,15 +44,10 @@ export default function MyAppBar() {
       <Toolbar />
       <Toolbar component="nav" variant="dense" className={classes.toolbarSecondary}>
         {sections.map(section => (
-          <Link
-            color="inherit"
-            noWrap
-            key={section}
-            variant="body2"
-            href="#"
-            className={classes.toolbarLink}
-          >
-            {section}
+          <Link href={section}>
+            <MLink color="inherit" noWrap key={section} variant="body2" href="#" className={classes.toolbarLink}>
+              {section}
+            </MLink>
           </Link>
         ))}
       </Toolbar>
