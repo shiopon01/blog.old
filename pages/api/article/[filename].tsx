de6a9565fde96adea8987e0091bc83306abf636d
shiopon01 <shio.0323@gmail.com>
2019-09-07 18:29:31 +0900

    updaaate api

--- pages/api/article/[filename].tsx
+++ pages/api/article/[filename].tsx
@@ -1,9 +1,13 @@
 import * as fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { any } from "prop-types";
 
-interface Response {
+interface Article {
   exists: boolean;
   title?: string;
+  body?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
 const handle = (req: NextApiRequest, res: NextApiResponse) => {
@@ -11,24 +15,36 @@ const handle = (req: NextApiRequest, res: NextApiResponse) => {
   const key: string | string[] = req.query.filename;
   const path: string = dir + key + ".md";
 
+  const response: Article = getArticle(path);
+
+  res.json(response);
+};
+
+const getArticle = (path: string): Article => {
+  const article: Article = {
+    exists: false
+  };
+
+  if (fileExists(path)) {
+    article.exists = true;
+  }
+
+  return article;
+};
+
+const fileExists = (path: string): boolean => {
   let exists: boolean = false;
 
   try {
     fs.statSync(path);
     exists = true;
   } catch (err) {
-    if (err.code === "ENOENT") {
-      exists = false;
-    }
+    // if (err.code === "ENOENT") {
+    //   exists = false;
+    // }
   }
 
-  const response: Response = {
-    exists: exists
-  };
-
-  response.title = "aa";
-
-  res.json(response);
+  return exists;
 };
 
 // const read = (path: string) => {
