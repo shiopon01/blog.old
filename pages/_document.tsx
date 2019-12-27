e974569c523f6e244b2a15a4be76e3a239a51191
shiopon01 <shio.0323@gmail.com>
2019-12-27 14:03:54 +0900

    fix: font default

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,8 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+          {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" /> */}
+          <link rel="stylesheet" href="/static/font.css" />
         </Head>
         <body>
           <Main />
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -2,25 +2,32 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Link from 'next/link'
-// import { makeStyles } from "@material-ui/core/styles";
+import Link from "next/link";
+import { makeStyles } from "@material-ui/core/styles";
 
-// const useStyles = makeStyles(theme => ({
-// }));
+const useStyles = makeStyles(() => ({
+  title: {
+    cursor: "pointer"
+  }
+}));
 
-export default function MyAppBar() {
-  // const classes = useStyles();
+const MyAppBar = () => {
+  const classes = useStyles();
 
   return (
     <React.Fragment>
       <AppBar>
         <Toolbar>
           <Link href="/">
-            <Typography variant="h6">SHIOPON BLOG</Typography>
+            <Typography variant="h6" className={classes.title}>
+              SHIOPON BLOG
+            </Typography>
           </Link>
         </Toolbar>
       </AppBar>
       <Toolbar />
     </React.Fragment>
   );
-}
+};
+
+export default MyAppBar;
